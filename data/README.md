772ce543b8f1a7955f1bcc304bc31a8855062854
clement <clement.demonchy@jobteaser.com>
2021-05-12 20:50:01 +0200

    feat(fetch script): add fetch script to download french embassies

---
+++ data/README.md
@@ -0,0 +1,1 @@
+folder for embassies files when downloading
---
+++ fetch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+set -x
+set -e
+wget https://www.data.gouv.fr/en/datasets/r/b629598e-df53-4915-b3c2-e70743c7fc34 -o data/embassies_french.csv

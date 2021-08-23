299926593e3f719a11e76756b0596b9a2ca41808
cdemonchy-pro <cdemonchy@talend.com>
2021-08-23 22:45:14 +0200

    WIP

--- create_access_point.sh
+++ create_access_point.sh
@@ -3,7 +3,9 @@ set -x
 set -e
 BUCKET_NAME=raw-embassies
 ACCOUNT_ID=`aws sts get-caller-identity | jq .Account --raw-output`
+# https://docs.aws.amazon.com/cli/latest/reference/s3control/create-access-point.html
 aws s3control create-access-point --account-id ${ACCOUNT_ID} --name ${BUCKET_NAME}-ap --bucket ${BUCKET_NAME}
+# https://docs.aws.amazon.com/cli/latest/reference/s3control/create-access-point-for-object-lambda.html
 aws s3control create-access-point-for-object-lambda --account-id ${ACCOUNT_ID} --name ${BUCKET_NAME}-ap-fol
 --configuration '{"SupportingAccessPoint": "${BUCKET_NAME}-ap","CloudWatchMetricsEnabled": true,"AllowedFeatures": ["GetObject-Range","GetObject-PartNumber"],"TransformationConfigurations": [
                 {
